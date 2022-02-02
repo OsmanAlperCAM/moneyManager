7b0efb374478862aa71f04df2de0aaacdefc858e
osmanalpercam <63802259+OsmanAlperCAM@users.noreply.github.com>
2022-02-02 12:44:40 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = props => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+export default App;
