527b9e1e292ea32bec46fe565759a241e89a5e6b
osmanalpercam <63802259+OsmanAlperCAM@users.noreply.github.com>
2022-02-09 11:42:14 +0300

    DashboardCard updated

--- src/Components/Cards/DashboardCard/DashboardCard.js
+++ src/Components/Cards/DashboardCard/DashboardCard.js
@@ -4,23 +4,16 @@ import {Card} from 'react-native-paper';
 import ColumnCard from './ColumnCard';
 import styles from './DashboardCard.style';
 
-const DashboardCard = ({
-  title,
-  content,
-  expense = 0,
-  income = 0,
-  cashFlow = 0,
-}) => {
+const DashboardCard = ({title, expense = 0, income = 0, balance = 0}) => {
   return (
     <View style={styles.container}>
       <View style={styles.innerContainer}>
         <Text style={styles.title}>{title}</Text>
-        <Text style={styles.content}>{content}$</Text>
+        <Text style={styles.content}>{balance}$</Text>
       </View>
       <View style={styles.bottomContainer}>
         <ColumnCard title={'Expense'} value={`${expense}$`} />
         <ColumnCard title="Income" value={`${income}$`} />
-        <ColumnCard title="Cash Flow" value={`${cashFlow}$`} />
       </View>
     </View>
   );
